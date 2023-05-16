3dc7c88d1870f91e35ebe1093cf2ec742056e13f
João Gabriel <128388946+JoaoGAO@users.noreply.github.com>
2023-05-16 22:09:55 +0000

    Resolução

--- main.py
+++ main.py
@@ -1,31 +1,33 @@
-grau=int(input("Digite o grau da equação:"))
+import math
 
-if grau<1 or grau>2:
+grau = float(input("Digite o grau da equação: "))
+
+if grau < 1 or grau > 2:
     print("Grau inválido")
 elif grau == 1:
     print("A equação é do primeiro grau")
-    a=float(input("Digite o valor de a:"))
-    if a==0:
+    a = float(input("Digite o valor de a: "))
+    if a == 0:
         print("Valor de a inválido")
     else:
-        b=float(input("Digite o valor de b:"))
-        x=-b/a
-        print(f"O valor da raiz é: {x:.2f}")
-else:
+        b = float(input("Digite o valor de b: "))
+        raiz = -b / a
+        print(f"O valor da raiz é: {raiz:.2f}")
+elif grau == 2:
     print("A equação é do segundo grau")
-    a=float(input("Digite o valor de a:"))
-    if a==0:
+    a = float(input("Digite o valor de a: "))
+    if a == 0:
         print("Valor de a inválido")
     else:
-        b=float(input("Digite o valor de b:"))
-        c=float(input("Digite o valor de c:"))
-        delta=b**2-4*a*c
-        if delta<0:
+        b = float(input("Digite o valor de b: "))
+        c = float(input("Digite o valor de c: "))
+        discriminante = b ** 2 - 4 * a * c
+        if discriminante < 0:
             print("A equação não possui raízes reais")
-        elif delta==0:
-            x=-b/(2*a)
-            print(f"A equação possui uma raiz real: {x:.2f}")
+        elif discriminante == 0:
+            raiz = -b / (2 * a)
+            print(f"A equação possui uma raiz real: {raiz:.2f}")
         else:
-            x1=(-b + delta**1/2)/(2*a)
-            x2=(-b - delta**1/2)/(2*a)
-            print(f"A equação possui duas raízes reais: {x1:.2f} e {x2:.2f}")
+            raiz1 = (-b + math.sqrt(discriminante)) / (2 * a)
+            raiz2 = (-b - math.sqrt(discriminante)) / (2 * a)
+            print(f"A equação possui duas raízes reais: {raiz1:.2f} e {raiz2:.2f}")
